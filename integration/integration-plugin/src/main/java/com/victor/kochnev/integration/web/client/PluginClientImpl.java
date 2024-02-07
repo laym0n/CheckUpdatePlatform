12272979e9cbcad6ea974b8375610fca9585c162
Виктор Кочнев <victor_k02@mail.ru>
2024-02-07 16:05:44 +0300

    add_webresource_for_observe добавил модуль с заглушкой PluginClient'а

---
+++ integration/integration-plugin/src/main/java/com/victor/kochnev/integration/web/client/PluginClientImpl.java
@@ -0,0 +1,26 @@
+package com.victor.kochnev.integration.web.client;
+
+import com.victor.kochnev.core.dto.plugin.WebResourcePluginDto;
+import com.victor.kochnev.core.integration.PluginClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+// TODO: 07.02.2024 реализовать класс
+@Component
+@RequiredArgsConstructor
+public class PluginClientImpl implements PluginClient {
+    @Override
+    public WebResourcePluginDto addResource(String baseUrl, String resourceDescription) {
+        return null;
+    }
+
+    @Override
+    public void removeResource(String resourceName) {
+
+    }
+
+    @Override
+    public WebResourcePluginDto getResourceState() {
+        return null;
+    }
+}
