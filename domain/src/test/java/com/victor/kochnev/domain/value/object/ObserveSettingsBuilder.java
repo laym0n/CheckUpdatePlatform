813d946a3f4669e90872f4af5377f31875daeca7
Виктор Кочнев <victor_k02@mail.ru>
2024-02-14 21:36:12 +0300

    удалил ненужное поле

--- domain/src/test/java/com/victor/kochnev/domain/value/object/ObserveSettingsBuilder.java
+++ domain/src/test/java/com/victor/kochnev/domain/value/object/ObserveSettingsBuilder.java
@@ -1,12 +1,8 @@
 package com.victor.kochnev.domain.value.object;
 
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
-
 public class ObserveSettingsBuilder {
     public static final String DEFAULT_EMAIL = "victor_k02@mail.ru";
     public static final String DEFAULT_TELEGRAM = "victor_k02";
-    public static final Duration DEFAULT_DURATION = Duration.of(10, ChronoUnit.DAYS);
 
     private ObserveSettingsBuilder() {
     }
