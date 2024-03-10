dfaccb6ccd2b0e5b188f27b55c8864bc7a974369
Виктор Кочнев <victor_k02@mail.ru>
2024-03-11 02:04:44 +0300

    send-notification правка контроллера

--- integration/integration-plugin/plugin-client/src/main/java/com/victor/kochnev/integration/plugin/controllers/NotificationController.java
+++ integration/integration-plugin/plugin-client/src/main/java/com/victor/kochnev/integration/plugin/controllers/NotificationController.java
@@ -1,8 +1,12 @@
 package com.victor.kochnev.integration.plugin.controllers;
 
+import com.victor.kochnev.core.dto.request.SendNotificationRequestDto;
 import com.victor.kochnev.core.facade.notification.NotificationFacade;
 import com.victor.kochnev.integration.plugin.api.NotificationApi;
 import com.victor.kochnev.integration.plugin.api.dto.NotificationCreateRequestBody;
+import com.victor.kochnev.integration.plugin.converter.PluginRequestMapper;
+import com.victor.kochnev.integration.plugin.security.entity.PluginSecurity;
+import com.victor.kochnev.integration.plugin.security.service.PluginSecurityService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -13,23 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 @Validated
 public class NotificationController implements NotificationApi {
     private final NotificationFacade notificationFacade;
+    private final PluginSecurityService pluginSecurityService;
+    private final PluginRequestMapper pluginRequestMapper;
 
     @Override
     public ResponseEntity<Void> create(NotificationCreateRequestBody requestBody) {
-//        PluginSecurity pluginSecurity = (PluginSecurity) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        SendNotificationRequestDto requestDto = new SendNotificationRequestDto();
-//
-//        NotificationPluginDto notificationPluginDto = new NotificationPluginDto();
-//        notificationPluginDto.setMessage(requestBody.getNotification().getMessage());
-//        requestDto.setNotificationDto(notificationPluginDto);
-//
-//        WebResourcePluginDto webResourcePluginDto = new WebResourcePluginDto();
-//        webResourcePluginDto.setName(requestBody.getUpdatedResource().getName());
-//        webResourcePluginDto.setDescription(requestBody.getUpdatedResource().getDescription());
-//        requestDto.setUpdatedWebResourceDto(webResourcePluginDto);
-//        requestDto.setPluginId(pluginSecurity.getId());
-//
-//        notificationFacade.sendNotification(requestDto);
+        SendNotificationRequestDto requestDto = pluginRequestMapper.mapToCore(requestBody);
+
+        PluginSecurity authenticatedPlugin = pluginSecurityService.getAuthenticatedPlugin();
+        requestDto.setPluginId(authenticatedPlugin.getId());
+
+        notificationFacade.sendNotification(requestDto);
         return ResponseEntity.ok().build();
     }
 }
--- integration/integration-plugin/plugin-client/src/main/java/com/victor/kochnev/integration/plugin/converter/PluginDtoMapper.java
+++ integration/integration-plugin/plugin-client/src/main/java/com/victor/kochnev/integration/plugin/converter/PluginDtoMapper.java
@@ -1,6 +1,10 @@
 package com.victor.kochnev.integration.plugin.converter;
 
 import com.victor.kochnev.core.dto.domain.entity.PluginDto;
+import com.victor.kochnev.core.dto.plugin.NotificationPluginDto;
+import com.victor.kochnev.core.dto.plugin.WebResourcePluginDto;
+import com.victor.kochnev.integration.plugin.api.dto.NotificationDto;
+import com.victor.kochnev.integration.plugin.api.dto.WebResourceDto;
 import com.victor.kochnev.integration.plugin.security.entity.PluginAuthority;
 import com.victor.kochnev.integration.plugin.security.entity.PluginSecurity;
 import org.mapstruct.Mapper;
@@ -17,4 +21,6 @@ import java.util.List;
 public interface PluginDtoMapper {
     @Mapping(target = "authorities", expression = "java(List.of(PluginAuthority.PLUGIN))")
     PluginSecurity mapToSecurity(PluginDto pluginDto);
+    WebResourcePluginDto mapToCore(WebResourceDto webResourceDto);
+    NotificationPluginDto mapToCore(NotificationDto notificationDto);
 }
