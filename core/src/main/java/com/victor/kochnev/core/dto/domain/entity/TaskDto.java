72728b4ea804067a6da62b2ece2888c06fa5dea4
Виктор Кочнев <victor_k02@mail.ru>
2024-05-04 16:37:20 +0300

    refactor добавил поля в TaskDto

--- core/src/main/java/com/victor/kochnev/core/dto/domain/entity/TaskDto.java
+++ core/src/main/java/com/victor/kochnev/core/dto/domain/entity/TaskDto.java
@@ -3,12 +3,14 @@ package com.victor.kochnev.core.dto.domain.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.victor.kochnev.core.dto.domain.value.object.PluginDescriptionTaskDto;
 import com.victor.kochnev.domain.enums.TaskDecision;
+import com.victor.kochnev.domain.enums.TaskType;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 
 @Data
@@ -19,12 +21,18 @@ public class TaskDto {
     @JsonProperty("id")
     @NotNull
     private UUID id;
+    @JsonProperty("createDate")
+    @NotNull
+    private ZonedDateTime createDate;
     @JsonProperty("description")
     @NotNull
     private PluginDescriptionTaskDto description;
     @JsonProperty("decision")
     @NotNull
     private TaskDecision decision;
+    @JsonProperty("type")
+    @NotNull
+    private TaskType type;
     @JsonProperty("comment")
     private String comment;
     @JsonProperty("plugin")
