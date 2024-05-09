bc9cc899dfb585ae8215db03b33e9aff7b7c41c7
Виктор Кочнев <victor_k02@mail.ru>
2024-05-09 16:45:38 +0300

    refactor

--- dal/src/main/resources/db/changelog/release/1.1.0/002_create_table_task.sql
+++ dal/src/main/resources/db/changelog/release/1.1.0/002_create_table_task.sql
@@ -7,12 +7,12 @@ CREATE TABLE IF NOT EXISTS TASK
 
     type                 VARCHAR(50)              NOT NULL,
     image_paths          VARCHAR(500),
-    description          VARCHAR(500),
+    description          VARCHAR(2000),
     distribution_methods VARCHAR(500)             NOT NULL,
     decision             VARCHAR(50),
     comment              VARCHAR(500),
     tags                 JSONB,
-    logo_path            VARCHAR(50),
+    logo_path            VARCHAR(500),
     plugin_id            UUID                     NOT NULL
 );
 
